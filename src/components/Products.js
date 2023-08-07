0f90621f24864b09e826dbbc71f1d39a79e74313
Mathius <info@yogimathius.dev>
2023-08-06 23:16:08 -0700

    feat: add products

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,5 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Products() {
-  return <h2>Products Page</h2>;
+  return (
+    <>
+      <h2>Products Page</h2>
+      <Link to="/products/1">Product #1</Link>
+      <Link to="/products/2">Product #2</Link>
+      <Link to="/products/3">Product #3</Link>
+      <Link to="/products/4">Product #4</Link>
+    </>
+  );
 }
