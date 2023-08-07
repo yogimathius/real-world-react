a5848e715f26e3a481a0d5076c057679767339cf
Mathius <info@yogimathius.dev>
2023-08-06 23:38:07 -0700

    feat: use mui button in Products

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,14 +1,24 @@
 import React from "react";
+import Button from "@mui/material/Button";
+
 import { Link } from "react-router-dom";
 
 export default function Products() {
   return (
     <>
       <h2>Products Page</h2>
-      <Link to="/products/1">Product #1</Link>
-      <Link to="/products/2">Product #2</Link>
-      <Link to="/products/3">Product #3</Link>
-      <Link to="/products/4">Product #4</Link>
+      <Link to="/products/1">
+        <Button>Product #1</Button>
+      </Link>
+      <Link to="/products/2">
+        <Button>Product #2</Button>
+      </Link>
+      <Link to="/products/3">
+        <Button>Product #3</Button>
+      </Link>
+      <Link to="/products/4">
+        <Button>Product #4</Button>
+      </Link>
     </>
   );
 }
