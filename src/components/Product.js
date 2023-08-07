ba74496508de078776ec43eb7d5fba53c31c8835
Mathius <info@yogimathius.dev>
2023-08-06 23:16:37 -0700

    feat: add dynamic rendering and back function

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,5 +1,18 @@
 import React from "react";
+import { useNavigate, useParams } from "react-router-dom";
 
 export default function Product() {
-  return <h2>Product Page</h2>;
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  const back = () => {
+    navigate("/products");
+  };
+
+  return (
+    <>
+      <h2>Product {id} Page</h2>
+      <button onClick={back}>Back</button>
+    </>
+  );
 }
