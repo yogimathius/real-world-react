2b796f86523d37ba5d011026d9a9a41819e5114d
Mathius <info@yogimathius.dev>
2023-08-07 21:56:11 -0700

    chore: add remove button to in between

--- src/context/InBetween.jsx
+++ src/context/InBetween.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Button from "./Button";
+import RemoveButton from "./RemoveButton";
 
 const InBetween = () => {
   return (
     <div>
       <h2>I'm in between!!!</h2>
       <Button />
+      <RemoveButton />
     </div>
   );
 };
